79e473829c9c683a421a52f74a4886a42462a0f0
SaltyChickenLeg <merijnjong1@gmail.com>
2025-09-25 17:33:44 +0200

    added http functionality

--- srcs/request.d
+++
@@ -1,2 +0,0 @@
-srcs/request.o: srcs/request.cpp srcs/../incs/request.hpp \
-  srcs/../incs/webserv.hpp srcs/../incs/utils.hpp
--- srcs/router.d
+++
@@ -1,3 +0,0 @@
-srcs/router.o: srcs/router.cpp srcs/../incs/router.hpp \
-  srcs/../incs/configStructs.hpp srcs/../incs/webserv.hpp \
-  srcs/../incs/utils.hpp srcs/../incs/request.hpp
